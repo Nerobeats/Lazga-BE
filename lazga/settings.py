c5e13a42bb9c7e59eed696062abed40f331457fe
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-04-13 12:04:11 +0300

    heroku blz

--- lazga/settings.py
+++ lazga/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
 import django_heroku
+import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "lazga_api",
+    'lazga_api',
     'rest_framework',
 ]
 
@@ -86,7 +86,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
     }
+    
 }
 
 
