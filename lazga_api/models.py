cab2e93758c8b09a5fb2c24aca5d17648c04bec0
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-04-14 15:00:33 +0300

    added price to the module , and choices for the type

--- lazga_api/models.py
+++ lazga_api/models.py
@@ -3,12 +3,34 @@ from django.contrib.auth.models import User
 
 
 class Item(models.Model):
+    
+    TSHIRT="RT"
+    HOODIE="HD"
+    MUG="MG"
+    POPSOCKET="PS"
+    POSTER="PR"
+    STICKER="ST"
+    COASTER="CR"
+
+    TYPES = [
+            (TSHIRT, 'tshirt'),
+            (HOODIE, 'hoodie'),
+            (MUG, 'mug'),
+            (POPSOCKET, 'popsocket'),
+            (POSTER, 'poster'),
+            (STICKER, 'sticker'),
+            (COASTER, 'coaster'),
+        ]
+    type = models.CharField(
+        max_length=2,
+        choices=TYPES,
+        default=TSHIRT,
+    )
     name = models.CharField(max_length=150)
     image_url = models.URLField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    type = models.CharField(max_length=50)
     tags = models.CharField(max_length=200)
+    itemPrice = models.DecimalField(max_digits=5, decimal_places=2, default= 17.00)
     added_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1 )
     def __str__(self):
         return self.name
-
