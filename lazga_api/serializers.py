cb9b21610cd5e4b8e288228e585a7e3cdec326ad
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-04-15 17:18:09 +0300

    serializer

--- lazga_api/serializers.py
+++ lazga_api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Item,Order
+from .models import Item
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
