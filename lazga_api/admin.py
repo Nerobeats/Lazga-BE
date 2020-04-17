596c007e211cff172787fd18508680108bcf0046
NeroBeats <Hani.Hamshari@Hotmail.com>
2020-04-17 20:20:53 +0300

    7aggak 3alena, e7na kona m5arbe6eeen

--- lazga_api/admin.py
+++ lazga_api/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Item, Order, OrderItem
-
+from .models import Item, Order, OrderItem,Type
 
 class OrderItemInLine(admin.TabularInline):
     model = OrderItem
